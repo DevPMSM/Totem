184b2b8701aabfab0a3aca6e43b0e9c75f99abbc
Luca.Freitas <luca.silva@edu.ufes.br>
2024-07-23 13:53:15 -0300

    feat: Criação de tela de Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,35 +1,30 @@
-<!--Linkando Reset Css -->
-<link rel="stylesheet" href="{{ asset('assets/css/reset.css') }}">
-<!-- Incluindo Design do Login -->
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!-- Linkando o Css -->
-<link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
-
-<div class="sidenav">
-    <div class="login-main-text">
-        <p>Entre com sua conta de administrador</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="page">
+        
+            
+            
     </div>
- </div>
- <div class="main">
-    <div class="col-md-6 col-sm-12">
-       <div class="login-form">
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input id="email" class="form-control" type="email" name="email" :value="old('email')" required autofocus autocomplete="email" />
-                </div>
-                <div class="form-group">
-                    <label for="password">Senha</label>
-                    <input id="password" class="form-control"
-                    type="password"
-                    name="password"
-                    required autocomplete="current-password" />
-                </div>
-                <button type="submit" class="btn btn-black">{{ __('Entrar') }}</button>
+    
+    <!-- <div class="page">
+    
+            <form method="POST" class="formLogin">
+                <h1>Login</h1>
+                <p>Digite os seus dados de acesso no campo abaixo.</p>
+                <label for="email">E-mail</label>
+                <input type="email" placeholder="Digite seu e-mail" autofocus="true" />
+                <label for="password">Senha</label>
+                <input type="password" placeholder="Digite seu e-mail" />
+                <a href="/">Esqueci minha senha</a>
+                <input type="submit" value="Acessar" class="btn" />
             </form>
-       </div>
-    </div>
- </div>
+    </div> -->
+</body>
+</html>
+
